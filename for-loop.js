41eedbbda1ddddd3b324bd2af395259122b98543
designer-sujon <cdnl2018@gmail.com>
2024-09-02 16:54:56 +0600

    firstt

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+const numbers = [72, 58, 12, 98, 100];
+
+console.log(numbers)
+
+for(const num of numbers){
+    console.log(num)
+}
---
+++ while-loop.js
@@ -0,0 +1,6 @@
+let num = 0; // loop variable
+
+while(num < 5){ // loop condition
+    console.log('looooping', num); // loop statement
+    num = num + 1; // loop variable change
+}
